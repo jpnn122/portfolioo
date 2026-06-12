50e2d58dd9ea43fe217d6c0616baf4c66f7f2413
Syphax Benchalal <s.benchalal@SB-FR.local>
2026-06-12 16:48:11 +0200

    Premier commit - correction bugs et ajouts

---
+++ src/env.d.ts
@@ -0,0 +1,14 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+declare module "@cv" {
+  const value: any;
+  export default value;
+  export const basics: any;
+  export const education: any;
+  export const work: any;
+  export const skills: any;
+  export const projects: any;
+  export const certificates: any;
+}
+
